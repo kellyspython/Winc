325ed2a78a9de32796c4b6c0e81b5cfd186d13b0
Kelly <kvanwijk@akemo.nl>
2023-01-11 11:42:41 +0100

    update

--- superpy/main.py
+++ superpy/main.py
@@ -1,8 +1,11 @@
 # Imports
+
 import random
 import argparse
 import csv
 from datetime import date
+import pandas as pd
+
 
 # Do not change these lines.
 __winc_id__ = "a2bc36ea784242e4989deb157d527ba0"
@@ -10,41 +13,48 @@ __human_name__ = "superpy"
 
 
 # Your code below this line.
+
 def main():
-    fields = ['id','Product Name', 'Count','Buy date', 'Buy Price', 'Expiration Date']
+
     filename = "bought.csv" 
     try:
         with open(filename, mode ='r')as file:
-            # reading the CSV file
-            csvFile = csv.reader(file)
-            # displaying the contents of the CSV file
-            for lines in csvFile:
-                pass
-                #print(lines)
-    
-    except FileNotFoundError:
 
+            csvFile = csv.reader(file)
+
+
+    except FileNotFoundError:
+        header = ['id','Product Name', 'Count','Buy date', 'Buy Price', 'Expiration Date']
+        
         with open(filename, 'w') as csvfile:
             csvwriter = csv.writer(csvfile)
-            csvwriter.writerow(fields)
+            csvwriter.writerow(header)
 
     def report():
-        pass
+        df = pd.read_csv(filename)
+        print(df.head())
+        # with open(filename, mode ='r')as file:
+        #     # reading the CSV file
+        #     csvFile = csv.reader(file)
+        #     # displaying the contents of the CSV file
+        #     for lines in csvFile:
+        #         print(lines)
     
+
     def test():
         global filename
-        # create the parser for the "foo" command
         
         parser = argparse.ArgumentParser(description='example Input "productname", "count","buy date", "buy_price", "exparation date" ')
-        parser.add_argument('product', type=str, help='Pruduct name')
+        parser.add_argument('id', type=int, default= random.randint(1000, 9999))
+        parser.add_argument('product', type=str, help='Product name')
         parser.add_argument('count', type=int, help='How many products have you bought')
         parser.add_argument('buy_date', type=str, help='When did you buy the product "date"')
-        parser.add_argument('price', type= int, help='price off the product you pay')
+        parser.add_argument('price', type= float, help='price off the product you pay')
         parser.add_argument('exparation', type= str, help='Whats the exparation date')
         args = parser.parse_args()
         
         shop_list = []
-        shop_list.append(random.randint(1000, 9999))
+        shop_list.append(args.id)
         shop_list.append(args.product)
         shop_list.append(args.count)
         shop_list.append(args.buy_date)
