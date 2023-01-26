4bdc44a4facf56e1620a48a783c6a02754f6482d
Kelly <kvanwijk@akemo.nl>
2023-01-26 11:55:23 +0100

    Update

---
+++ templates/main.py
@@ -0,0 +1,19 @@
+from flask import Flask, render_template, redirect, url_for
+
+__winc_id__ = "9263bbfddbeb4a0397de231a1e33240a"
+__human_name__ = "templates"
+
+app = Flask(__name__)
+
+
+@app.route('/home')
+def hello():
+    return redirect("http://localhost/", 302)
+
+@app.route("/")
+def index():
+    return b"<title>Index</title>"
+
+@app.route("/about")
+def about():
+    return b"<title>About</title>"
